b878c36ca0d1bbc0ce825ff57c95b7eee81274af
mrozo2m <mrozo2m@interia.pl>
2019-04-04 12:24:02 +0200

    changed props for user in ResultsItem.js

--- my-app/src/List/Results/ResultItsem.js
+++ my-app/src/List/Results/ResultItsem.js
@@ -1,8 +1,8 @@
 import React from 'react'
 
-const ResultsItem = (props) => (
+const ResultsItem = ({user}) => (
     <div>
-        {props.user.name.first} {props.user.name.last}
+        {user.name.first} {user.name.last}
     </div>
 
 )
